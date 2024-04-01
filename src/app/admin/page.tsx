fa407344e707994dac5a86890c320b2c33406e11
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2024-04-01 19:29:19 +0200

    add user form

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,152 @@
+/* eslint-disable import/prefer-default-export */
+
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  firstName: z.string().min(2).max(50),
+  lastName: z.string().min(2).max(50),
+  email: z.string().email({ message: "Invalid email address" }).max(50),
+  role: z.enum(["user", "admin"]),
+});
+
+// const roles = [
+//   {
+//     value: "user",
+//     label: "User",
+//   },
+//   {
+//     value: "admin",
+//     label: "Admin",
+//   },
+// ];
+
+export default function AddUser() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      role: "user",
+    },
+  });
+
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+    try{
+      const response = await fetch("/api/add-user",{
+        method:"POST",
+        body: JSON.stringify(values),
+        headers: {
+          "Content-Type": "application/json"
+        },
+
+      },
+      
+      // lets do some better error handling / return if successful
+      )
+      return response.json()
+    }
+    catch(error) {
+      console.error(error)
+    throw error 
+    }
+  }
+
+  return (
+    <div className="flex flex-col p-8">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="firstName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>First Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="John" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="lastName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Last Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Smith" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="js@gmail.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="role"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  >
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Theme" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="user">User</SelectItem>
+                      <SelectItem value="admin">Admin</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
---
+++ src/app/api/add-user/route.ts
@@ -0,0 +1,20 @@
+import { createClient } from "@libsql/client";
+import { NextResponse } from "next/server";
+
+
+
+const client = createClient({
+  url: process.env.DATABASE_URL!,
+  authToken: process.env.DATABASE_AUTH_TOKEN,
+});
+
+// eslint-disable-next-line import/prefer-default-export
+export async function POST(request: Request){
+    const { firstName, lastName, email, role } = await request.json()
+    console.log(firstName, lastName, email, role)
+    
+    await client.execute(`INSERT INTO APPUSERS (firstName, lastName, email, role) VALUES ("${firstName}", "${lastName}", "${email}", "${role}")`);
+
+    return NextResponse.json("success");
+
+}
